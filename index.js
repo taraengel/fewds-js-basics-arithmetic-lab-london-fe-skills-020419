612583afdb2ec0b6fcae33b48568df15ef77b88a
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-11 19:09:13 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+const baseOffset = 1000000000
+
+const newID = baseOffset + oldID;
+
+const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumber = Math.random(0, 20);
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + baseOffset + 1;
+
