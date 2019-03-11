eeb058acb17f27650f40f629371f8f902a974a1e
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-11 19:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-const baseO ffset= 1000000000
+const baseOffset = 1000000000
 
-const newID = 1000000000 + oldID;
+const newID = baseOffset + oldID;
 
 const ageIsValid = Number.isInteger(currentAge);
 
